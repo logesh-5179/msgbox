656494e6846fb1985ef60114f1d115d67cfd8154
logesh-5179 <logeshmb5179@gmail.com>
2024-07-23 20:01:18 +0530

    Update script.js
    I have forgotten to add break statement in line-13..So I have updated

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,28 @@
-const ask=[
-        "What is your name?",
-        "What is your fav place?",
-        "What is your hobby?"
-    ]
-function acc()
-{
+const ask = [
+    "What is your name?",
+    "What is your fav place?",
+    "What is your hobby?"
+];
+
+function acc() {
     const input = document.getElementById("inp").value;
     let now = false;
-    for(let i=0;i<ask.length;i++)
-    {
-        if(ask[i]==inp)
-        now=true;
-    
+    for (let i = 0; i < ask.length; i++) {
+        if (ask[i] === input) {
+            now = true;
+            break;
+        }
+    }
+
     const con = document.getElementById("Ask");
-    if(now)
-    {
-        con.style.backgroundColor="green";
+    if (now) {
+        con.style.backgroundColor = "green";
+    } else {
+        con.style.backgroundColor = "red";
     }
-    else
-    {
-        con.style.backgroundColor="red";
-    }
-}
 }
 
-function dec()
-{
-    const input=document.getElementById("inp")
-    inp.value="";
+function dec() {
+    const input = document.getElementById("inp");
+    input.value = "";
 }
